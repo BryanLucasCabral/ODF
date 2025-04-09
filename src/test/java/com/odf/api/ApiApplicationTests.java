4884bae0bfbec7bf7f1dd30eb1178b6521baaf7e
BryanLucasCabral <bryan.brasil.bl@gmail.com>
2025-04-09 19:36:26 -0300

    Projeto criado

---
+++ src/test/java/com/odf/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odf.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
