4785135438d20c191ae54c71b2432a2eae92ff62
BryanLucasCabral <bryan.brasil.bl@gmail.com>
2025-04-27 21:04:03 -0300

    Adicionado dtos

--- src/main/java/com/odf/api/dto/usuarios/OdfUsuarioGenericoDTO.java
+++ src/main/java/com/odf/api/dto/usuarios/OdfUsuarioGenericoDTO.java
@@ -3,18 +3,21 @@ package com.odf.api.dto.usuarios;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
 import java.util.Date;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class OdfUsuarioDTO {
+public class OdfUsuarioGenericoDTO {
     private String nome;
     private String email;
     private String cpf;
     private String telefone;
     private String celular;
-    private Date dataNascimento;
+    private LocalDate dataNascimento;
+    private String sexo;
     private String cro;
     private String especialidade;
     private Boolean status;
